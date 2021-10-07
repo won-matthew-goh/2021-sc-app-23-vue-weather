b6782ef2c3577fb6ea8154dfe279796e2dad1091
won-matthew-goh <qimo2000@naver.com>
2021-10-07 16:10:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from './components/inc/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/inc/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header-wrapper">
+    <h1 class="logo">
+      <BIconCompass class="icon" />
+      날씨앱
+      <small>v 0.1</small>
+    </h1>
+    <BBreadcrumb :items="items" />
+  </header>
+</template>
+
+<script>
+import { BIconCompass, BBreadcrumb } from 'bootstrap-vue'
+
+export default {
+  name: 'Header',
+  components: {
+    BIconCompass,
+    BBreadcrumb
+  },
+  data () {
+    return {
+      items: [
+        { text: 'Daily', href: '/' },
+        { text: '5Days', href: '/5days' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper {
+  @include flex($h: space-between);
+  background-color: $dark-color;
+  color: $light-color;
+  padding: .5em;
+  font-size: 1.5em;
+}
+</style>
